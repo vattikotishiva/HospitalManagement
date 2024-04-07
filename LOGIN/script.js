73281fa4bdb7bf29379ea2b411e02254d8153be3
vattikoti shiva <vattikotishiva77@gmail.com>
2024-04-07 23:27:12 +0530

    first commit

--- LOGIN/script.js
+++ LOGIN/script.js
@@ -1,8 +1,11 @@
  // Define an array of user credentials
+ 
+
+
  var usersData = [
     { username: "admin", password: "password1" },
     { username: "shiva", password: "123456" },
-    { username: "santhosh", password: "123456" },
+    { username: "santhosh", password: "123456.0 " },
     // Add more user credentials as needed
 ];
 
@@ -28,3 +31,31 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
         alert("Invalid username or password. Please try again.");
     }
 });
+
+
+document.getElementById('regisForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent default form submission
+
+    // Get username and password from form input
+    var username = document.getElementById('username1').value;
+    var password = document.getElementById('password1').value;
+    sessionStorage.setItem('username',username)
+
+    // Check each set of user credentials
+    var isAuthenticated = usersData.some(function(user) {
+        return user.username === username && user.password === password;
+    });
+
+    // If any of the user credentials match, redirect to home page
+    if (isAuthenticated) {
+        window.location.href = "/index.html";
+    } else {
+        // Display error message or perform other actions for failed login attempt
+        alert("Invalid username or password. Please try again.");
+    }
+});
+
+
+
+
+
